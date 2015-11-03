1f173c8378414b3c847db1f90bf3b739893158d3
safari <sshsfl@yeah.net>
2015-11-03 18:56:20 +0800

    add article index

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,9 +3,32 @@
 namespace App\Http\Controllers;
 
 use DB;
+use App\Exceptions\ValidationException;
 
-class ArticleController extends Controller
+class ArticleController extends CommonController
 {
+    public function index()
+    {
+        $pictureNews = $this->article()
+            ->where('article_havelogo', 1)
+            ->orderBy('article_addtime', 'desc')
+            ->take(3)
+            ->get();
+
+        foreach ($pictureNews as $value) {
+            $value->thumbnail_url = $this->addImagePrefixUrl($value->thumbnail_url);
+        }
+
+        // $columns = $this->getColumns();
+
+        // foreach ($columns as $column) {
+        //     // 获取栏目下的文章列表
+        //     $column->articles = $this->getColumnArticle($column->column_id);
+        // }
+
+        return ['picture_news' => $pictureNews];
+        // return ['picture_news' => $pictureNews, 'article_list' => $columns];
+    }
     public function report()
     {
         return DB::connection('sqlsrv')->table('lanmu')
