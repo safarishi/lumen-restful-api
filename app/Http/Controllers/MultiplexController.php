65119f7145255c96166c2ebe45e24191335e3dd0
safari <sshsfl@yeah.net>
2015-11-04 16:35:27 +0800

    新增 上传用户头像静态方法

---
+++ app/Http/Controllers/MultiplexController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Request;
+use Intervention\Image\Facades\Image;
+
+class MultiplexController extends Controller
+{
+    /**
+     * 上传用户头像
+     *
+     * @param string $uid 用户id
+     */
+    public static function uploadAvatar($uid)
+    {
+        $ext = 'png';
+        $subDir = substr($uid, -1);
+        $storageDir = config('imagecache.paths.avatar_url').'/'.$subDir.'/';
+        $storageName = $uid;
+        $storagePath = $subDir.'/'.$storageName.'.'.$ext;
+
+        if (!file_exists($storageDir)) {
+            @mkdir($storageDir, 0777, true);
+        }
+
+        Image::make(Request::file('avatar_url'))
+            ->encode($ext)
+            ->save($storageDir.$storageName.'.'.$ext);
+
+        return config('imagecache.paths.avatar_url_prefix').'/'.$storagePath;
+    }
+}
